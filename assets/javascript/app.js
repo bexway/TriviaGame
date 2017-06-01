00f4ff2ebddfb5020c97fdbccc2f2b1fdf1c5e8f
Rebecca Way <rebeccaway2016@u.northwestern.edu>
2017-06-01 18:41:45 -0500

    master - object skeleton created

---
+++ assets/javascript/app.js
@@ -0,0 +1,18 @@
+var game = {
+  currentQuestion: "",
+  currentCorrectAnswer: -1,
+  currentOptions: [],
+  questiondb: [],
+  wins: 0,
+  losses: 0,
+  timer:20,
+  interval:null,
+
+  startTimer: function(){
+    console.log('woah');
+  },
+
+
+
+
+};
