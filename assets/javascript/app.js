5cd0e856288bbd1e8854185577c6303194af149e
Rebecca Way <rebeccaway2016@u.northwestern.edu>
2017-06-01 19:14:23 -0500

    master - basic timer functions added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,14 +5,50 @@ var game = {
   questiondb: [],
   wins: 0,
   losses: 0,
-  timer:20,
+  defaultAnswerTime:20,
+  timeLeft:20,
   interval:null,
+  timerStarted: 0,
 
   startTimer: function(){
     console.log('woah');
+    if (!this.timerStarted) {
+      this.timeLeft = this.defaultAnswerTime;
+      this.interval = setInterval(this.updateTimer, 1000);
+      this.timerStarted = true;
+    }
+    else{
+      clearInterval(this.interval);
+      this.timerStarted = false;
+    }
   },
+
+  updateTimer: function(){
+    game.timeLeft--;
+    console.log(game.timeLeft);
+  },
+
+  createQuestionHTML: function(){
+    console.log('placeholder');
+    //Create input of radio type, put them all in a giv and change the display to make them inline
+  },
+
+  ifRightAnswer: function(){
+    console.log('placeholder');
+    //increment wins
+    //stop timer
+    //check if done with trivia
+    //setTimeout(newQuestionFunction, 1000 * 5);
+    //to give a few seconds before next question
+  }
 
 
 
 
 };
+
+$(document).ready(function() {
+  $('#timerbutton').click(function(){
+    game.startTimer();
+  });
+});
