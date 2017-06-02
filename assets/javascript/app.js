06dd132017fe0eab1cf3072512b506be1751430e
Rebecca Way <rebeccaway2016@u.northwestern.edu>
2017-06-01 19:38:42 -0500

    master - general random number, serial array, and shuffle functions added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,8 @@ var game = {
   currentQuestion: "",
   currentCorrectAnswer: -1,
   currentOptions: [],
-  questiondb: [],
+  questiondb: questiondb,
+  //An array of objects
   wins: 0,
   losses: 0,
   defaultAnswerTime:20,
@@ -23,9 +24,19 @@ var game = {
     }
   },
 
+  stopTimer: function(){
+    clearInterval(this.interval);
+    this.timerStarted = false;
+  },
+
   updateTimer: function(){
     game.timeLeft--;
     console.log(game.timeLeft);
+  },
+
+  nextQuestion: function(){
+    var q = this.questiondb[randomNumber(this.questiondb.length)];
+    console.log(q.question);
   },
 
   createQuestionHTML: function(){
@@ -40,12 +51,60 @@ var game = {
     //check if done with trivia
     //setTimeout(newQuestionFunction, 1000 * 5);
     //to give a few seconds before next question
+  },
+
+  ifWrongAnswer: function(){
+    console.log('placeholder');
+    //increment losses
+    //stop timer
+    //check if done with trivia
+    //setTimeout(newQuestionFunction, 1000 * 5);
+    //to give a few seconds before next question
+  },
+
+  gameOver: function(){
+    console.log('placeholder');
+    //display stats
+    //ask to start new game
   }
 
 
 
 
 };
+
+function randomNumber(max, min=0){
+  return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+function serialArray(n){
+  var arr = [];
+  for (var i = 1; i <= n; i++) {
+   arr.push(i);
+  }
+  return arr;
+}
+
+
+//from: https://stackoverflow.com/a/2450976
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
 
 $(document).ready(function() {
   $('#timerbutton').click(function(){
---
+++ assets/javascript/questiondb.js
@@ -0,0 +1,12 @@
+questiondb = [
+  {
+    question:"what",
+    answer:["0","1","2","3"],
+    correctAnswer: "0"
+  },
+  {
+    question:"what",
+    answer:["0","1","2","3"],
+    correctAnswer: "0"
+  }
+];
